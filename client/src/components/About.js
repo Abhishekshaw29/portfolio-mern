09bba663b2ef996fab263629dd6d7a6b5308551b
Abhishek shaw <abhishekshaw813@gmail.com>
2022-02-09 03:51:01 -0800

    make it better

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -44,82 +44,85 @@ const About = () => {
     
     return (
         <>
-            <div className="container">
-                <div className="main-body">
-
-                    <div className="row gutters-sm">
-                        <div className="col-md-4 mb-3">
-                            <div className="card">
-                                <div className="card-body">
-                                    <div className="d-flex flex-column align-items-center text-center">
-                                        <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Admin" className="rounded-circle" width="150" />
-                                        <div className="mt-3">
-                                            <h4>Abhishek shaw</h4>
-                                            <p className="text-secondary mb-1">Full Stack Developer</p>
-                                            <p className="text-muted font-size-sm">St.thomas college of engineering and technology</p>
-                                            <NavLink to="/contact" ><button className="btn btn-primary">Message</button></NavLink>
-                                            <hr />
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div>
-                        <div className="col-md-8">
-                            <div className="card mb-3">
-                                <div className="card-body">
-                                    <div className="row">
-                                        <div className="col-sm-3 mb-4">
-                                            <h6 className="mb-0">Full Name</h6>
-                                        </div>
-                                        <div className="col-sm-9 text-secondary">
-                                            {data.name}
-                                        </div>
-                                    </div>
-                                    <hr />
-                                    <div className="row">
-                                        <div className="col-sm-3">
-                                            <h6 className="mb-0">Email</h6>
-                                        </div>
-                                        <div className="col-sm-9 text-secondary">
-                                            {data.email}
-                                        </div>
-                                    </div>
-                                    <hr />
-                                    <div className="row">
-                                        <div className="col-sm-3">
-                                            <h6 className="mb-0">Phone</h6>
-                                        </div>
-                                        <div className="col-sm-9 text-secondary">
-                                            {data.phone}
-                                        </div>
-                                    </div>
-                                    <hr />
-                                    <div className="row">
-                                        <div className="col-sm-3">
-                                            <h6 className="mb-0">Work</h6>
-                                        </div>
-                                        <div className="col-sm-9 text-secondary">
-                                            {data.work}
-                                        </div>
-                                    </div>
-                                    <hr />
-                                    <div className="row">
-                                        <div className="col-sm-12">
-                                            <button className="btn btn-outline-dark">Edit</button>
-
-                                        </div>
-                                    </div>
-                                </div>
-
-                            </div>
-
-                        </div>
-                    </div>
-
-                </div>
-            </div>
+        hello contact
         </>
+        // <>
+        //     <div className="container">
+        //         <div className="main-body">
+
+        //             <div className="row gutters-sm">
+        //                 <div className="col-md-4 mb-3">
+        //                     <div className="card">
+        //                         <div className="card-body">
+        //                             <div className="d-flex flex-column align-items-center text-center">
+        //                                 <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Admin" className="rounded-circle" width="150" />
+        //                                 <div className="mt-3">
+        //                                     <h4>Abhishek shaw</h4>
+        //                                     <p className="text-secondary mb-1">Full Stack Developer</p>
+        //                                     <p className="text-muted font-size-sm">St.thomas college of engineering and technology</p>
+        //                                     <NavLink to="/contact" ><button className="btn btn-primary">Message</button></NavLink>
+        //                                     <hr />
+        //                                 </div>
+        //                             </div>
+        //                         </div>
+        //                     </div>
+
+        //                 </div>
+        //                 <div className="col-md-8">
+        //                     <div className="card mb-3">
+        //                         <div className="card-body">
+        //                             <div className="row">
+        //                                 <div className="col-sm-3 mb-4">
+        //                                     <h6 className="mb-0">Full Name</h6>
+        //                                 </div>
+        //                                 <div className="col-sm-9 text-secondary">
+        //                                     {data.name}
+        //                                 </div>
+        //                             </div>
+        //                             <hr />
+        //                             <div className="row">
+        //                                 <div className="col-sm-3">
+        //                                     <h6 className="mb-0">Email</h6>
+        //                                 </div>
+        //                                 <div className="col-sm-9 text-secondary">
+        //                                     {data.email}
+        //                                 </div>
+        //                             </div>
+        //                             <hr />
+        //                             <div className="row">
+        //                                 <div className="col-sm-3">
+        //                                     <h6 className="mb-0">Phone</h6>
+        //                                 </div>
+        //                                 <div className="col-sm-9 text-secondary">
+        //                                     {data.phone}
+        //                                 </div>
+        //                             </div>
+        //                             <hr />
+        //                             <div className="row">
+        //                                 <div className="col-sm-3">
+        //                                     <h6 className="mb-0">Work</h6>
+        //                                 </div>
+        //                                 <div className="col-sm-9 text-secondary">
+        //                                     {data.work}
+        //                                 </div>
+        //                             </div>
+        //                             <hr />
+        //                             <div className="row">
+        //                                 <div className="col-sm-12">
+        //                                     <button className="btn btn-outline-dark">Edit</button>
+
+        //                                 </div>
+        //                             </div>
+        //                         </div>
+
+        //                     </div>
+
+        //                 </div>
+        //             </div>
+
+        //         </div>
+        //     </div>
+        // </>
     )
 }
 
